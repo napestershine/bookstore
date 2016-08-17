aae979b5e5b57be1da8d99ecabbfdb04cf8bcd45
root <napestershine@gmail.com>
2016-08-17 17:25:13 +0530

    created seeder for users table

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,3 +5,27 @@
  * Date: 17/8/16
  * Time: 5:17 PM
  */
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array(
+            'email' => 'member@email.com',
+            'password' => Hash::make('password'),
+            'name' => 'Manoj Kumar',
+            'admin' => 0
+        ));
+
+        User::create(array(
+            'email' => 'admin@email.com',
+            'password' => Hash::make('adminpassword'),
+            'name' => 'admin',
+            'admin' => 1
+        ));
+    }
+}
